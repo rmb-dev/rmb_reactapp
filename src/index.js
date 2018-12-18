8d53d1f5e53d1a7a9a39a7b8ccd6a1e1ac3b4e63
Roman <rombyr@outlook.com>
2018-12-18 00:05:38 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,7 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Hello() {
+  let phrase = "React"
+  return (
+    <h1> This is {phrase} App </h1>
+  )
+}
+
+function SayFullName(props) {
+  return (
+    <div>
+    <h1> My name {props.name}, last name - {props.surname} </h1>
+    <a href={props.link}> Link to my profile  </a>
+    </div>
+  )
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+registerServiseWorker();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
