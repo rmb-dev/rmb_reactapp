723220579e3dbd6f0a09b31bec042cd72817b1e4
Roman <rombyr@outlook.com>
2019-04-10 16:33:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 
-import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
-import styled from 'styled-components'
-
-class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <SayFullName name="Inst" surname=" " link="https://www.instagram.com/"/>
-        <SayFullName name="Face" surname=" " link="https://www.facebook.com"/>
-        <SayFullName name="Twit" surname=" " link="https://www.twitter.com"/>
-      </div>
-    );
-  }
-}
-
-function SayFullName(props) {
-  return (
-    <div>
-    <h1>My name {props.name}, last name - {props.surname}</h1>
-    <a href={props.link}> Link to my profile  </a>
-    </div>
-  )
-}
 
 const API = 'https://icanhazdadjoke.com';
 
@@ -54,10 +28,10 @@ const Joke = () => {
   return (
     <div>
      <p>{data.joke}</p>
-     <button onClick={refetch}>Another<button> 
+      <button onClick={refetch}>Another</button> 
     </div>
     )
 }
 
 
-export default App;
+export default useDad;
